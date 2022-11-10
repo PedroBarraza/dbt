b57ee581dbc68d2c2661c28698d9d284537c6f21
Pedro Barraza <pedropaschbarraza@gmail.com>
2022-11-10 11:31:55 -0300

    First login, hid token

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+const { Client, Events, GatewayIntent, GatewayIntentBits } = require('discord.js');
+const { token }= require('./config.json');
+
+console.log(token);
+
+const client = new Client({intents: [GatewayIntentBits.Guilds]});
+
+client.once(Events.ClientReady, c => {
+    console.log(`Logged in as ${c.user.tag}`);
+});
+
+client.login(token);

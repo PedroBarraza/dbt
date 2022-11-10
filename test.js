198782a1008a0a865b21bf5f3347996bc935b1ad
Pedro Barraza <pedropaschbarraza@gmail.com>
2022-11-10 12:25:25 -0300

    add ping command

--- test.js
+++ test.js
@@ -1,7 +1,24 @@
+const fs = require('node:fs');
+const path = require('node:path');
 const { Client, Events, GatewayIntent, GatewayIntentBits } = require('discord.js');
 const { token }= require('./config.json');
 
 console.log(token);
+
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ('data' in command && 'execute' in command) { //read up on this a bit better.
+        client.commands.set(command.data.name, command);
+    } else {
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
+}
 
 const client = new Client({intents: [GatewayIntentBits.Guilds]});
 

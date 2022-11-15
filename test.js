85b642fe18a2e77a9a000583646ed4167d538d6e
Pedro Barraza <pedropaschbarraza@gmail.com>
2022-11-15 11:02:49 -0300

    Add avatar command and deploy script

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Events, GatewayIntent, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, Events, GatewayIntent, GatewayIntentBits } = require('discord.js');
 const { token }= require('./config.json');
+
+const client = new Client({intents: [GatewayIntentBits.Guilds]});
 
 console.log(token);
 
@@ -13,17 +15,33 @@ const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('
 for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
     const command = require(filePath);
-    if ('data' in command && 'execute' in command) { //read up on this a bit better.
+    if ('data' in command && 'execute' in command) {
         client.commands.set(command.data.name, command);
     } else {
         console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
     }
 }
 
-const client = new Client({intents: [GatewayIntentBits.Guilds]});
-
 client.once(Events.ClientReady, c => {
     console.log(`Logged in as ${c.user.tag}`);
 });
 
 client.login(token);
+
+client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+    console.log(interaction);
+    
+    const command = interaction.client.commands.get(interaction.commandName);
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName}was found.`);
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    }
+});

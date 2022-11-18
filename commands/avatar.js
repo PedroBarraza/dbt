0860ace20154956470633210898043cee09e6e9e
Pedro Barraza <pedropaschbarraza@gmail.com>
2022-11-17 21:48:46 -0300

    Add connection command

--- commands/avatar.js
+++ commands/avatar.js
@@ -6,6 +6,8 @@ module.exports = {
         .setDescription('Get the avatar URL of the selected user, or your own avatar.')
         .addUserOption(option => option.setName('target').setDescription('The user\'s avatar to show')),
     async execute(interaction) {
+        const member = guild.members.cache.get(interaction.member.user.id);
+        const voiceChannel = member.voice.channel;
         const user = interaction.options.getUser('target');
         if (user) return interaction.reply(`${user.username}'s avatar: ${user.displayAvatarURL()}`);
         return interaction.reply(`Your avatar: ${interaction.user.displayAvatarURL()}`);
---
+++ commands/connect.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder, ChannelType } = require('discord.js');
+const { joinVoiceChannel } = require('@discordjs/voice');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('connect')
+        .setDescription('Connects to your voice channel of choice')
+        .addChannelOption(option => 
+            option
+            .setName('channel')
+            .setDescription('Voice channel to join.')
+            .setRequired(true)
+            .addChannelTypes(ChannelType.GuildVoice)),
+    async execute(interaction) {
+        const voiceChannel = interaction.options.getChannel('channel');
+        const voiceConnection = joinVoiceChannel({
+            channelId: voiceChannel.id,
+            guildId: interaction.guildId,
+            adapterCreator: interaction.guild.voiceAdapterCreator,
+        });
+    },
+};
